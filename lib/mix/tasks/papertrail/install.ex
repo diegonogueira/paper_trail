d9f461a395c14658f2b19aa6f5504c771539e308
Diego Nogueira <diegoalvareznogueira@gmail.com>
2016-12-27 09:46:08 -0200

    papertrail install changed

--- lib/mix/tasks/papertrail/install.ex
+++ lib/mix/tasks/papertrail/install.ex
@@ -2,32 +2,22 @@ defmodule Mix.Tasks.Papertrail.Install do
   @shortdoc "generates paper_trail migration file for your database"
 
   import Macro, only: [underscore: 1]
+  import Mix.Ecto
   import Mix.Generator
 
   use Mix.Task
 
-  def run(_args) do
-    path = Path.relative_to("priv/repo/migrations", Mix.Project.app_path)
+  def run(args) do
+    repo = args |> parse_repo |> List.first
+    ensure_repo(repo, args)
+
+    path = Path.relative_to(migrations_path(repo), Mix.Project.app_path)
     file = Path.join(path, "#{timestamp()}_#{underscore(AddVersions)}.exs")
     create_directory path
 
-    create_file file, """
-    defmodule Repo.Migrations.AddVersions do
-      use Ecto.Migration
+    assigns = [mod: Module.concat([repo, Migrations, AddVersions])]
 
-      def change do
-        create table(:versions) do
-          add :event,        :string
-          add :item_type,    :string
-          add :item_id,      :integer
-          add :item_changes, :map
-          add :meta,         :map
-
-          add :inserted_at,  :datetime, null: false
-        end
-      end
-    end
-    """
+    create_file file, migration_template(assigns)
   end
 
   defp timestamp do
@@ -37,4 +27,22 @@ defmodule Mix.Tasks.Papertrail.Install do
 
   defp pad(i) when i < 10, do: << ?0, ?0 + i >>
   defp pad(i), do: to_string(i)
+
+  embed_template :migration, """
+  defmodule <%= inspect @mod %> do
+    use Ecto.Migration
+
+    def change do
+      create table(:versions) do
+        add :event,        :string
+        add :item_type,    :string
+        add :item_id,      :integer
+        add :item_changes, :map
+        add :meta,         :map
+
+        add :inserted_at,  :datetime, null: false
+      end
+    end
+  end
+  """
 end
